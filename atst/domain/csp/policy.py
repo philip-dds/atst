002df6041555e67804b8e52100510cf85af0ed4a
dandds <dan-ctr@friends.dds.mil>
2019-12-13 16:32:46 -0500

    wip: Initial policies, internal method for creating policy definition

---
+++ atst/domain/csp/policy.py
@@ -0,0 +1,13 @@
+class AzurePolicyManager:
+    def __init__(self, static_policy_location):
+        self._static_policy_location = static_policy_location
+        pass
+
+    def portfolio_definitions():
+        pass
+
+    def application_definitions():
+        pass
+
+    def environment_definitions():
+        pass
